578f03c546eff1a098ad1e8270166a1a03bd6c7f
Lalka00pq <lalka00pq@gmail.com>
2025-04-15 15:55:28 +0300

    Added new models

--- src/routers/api_interface_image/router.py
+++ src/routers/api_interface_image/router.py
@@ -8,7 +8,7 @@ from PIL import Image
 from pydantic import TypeAdapter
 
 import torch
-
+from torchvision import transforms
 import ultralytics
 
 
@@ -32,29 +32,30 @@ service_config_python = service_config_adapter.validate_python(
 router = APIRouter(tags=["Main FastAPI service router"], prefix="")
 
 
-# def preprocess_image(image_path: str) -> np.ndarray:
-#     """Предобработка изображения для классификатора
+def preprocess_image(image_path: str) -> np.ndarray:
+    """Предобработка изображения для классификатора
 
-#     Args:
-#         image_path (str): Путь к изображению
+    Args:
+        image_path (str): Путь к изображению
 
-#     Returns:
-#         np.ndarray: массив изображения
-#     """
-#     input_image = Image.open(image_path).convert('RGB')
-#     preprocess = transforms.Compose([
-#         transforms.Resize(256),
-#         transforms.CenterCrop(224),
-#         transforms.ToTensor(),
-#         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
-#                              0.229, 0.224, 0.225]),
-#     ])
-#     input_tensor = preprocess(input_image)
-#     input_batch = input_tensor.unsqueeze(0)
-#     return input_batch.numpy()
+    Returns:
+        np.ndarray: массив изображения
+    """
+    input_image = Image.open(image_path).convert('RGB')
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
+                             0.229, 0.224, 0.225]),
+    ])
+    input_tensor = preprocess(input_image)
+    input_batch = input_tensor.unsqueeze(0)
+    return input_batch.numpy()
 
 
 class ModelYolo:
+    # TODO: добавить возможность выбора конфигурации модели
     def __init__(self, model_path: str = 'src/models/detectors/trained models/yolo11n-pose',
                  device: str = 'cpu',
                  model_type: str = 'pt') -> None:
@@ -97,7 +98,7 @@ class ModelYolo:
             Keypoints_yolo_models: Ключевые точки модели
         """
         image_for_detect = Image.open(
-            io.BytesIO(image.file.read())).convert('RGB').resize((640, 640))
+            io.BytesIO(image.file.read())).convert('RGB')
         if self.model_type == 'onnx':
             results = self.model(image_for_detect, device=self.device)
         elif self.model_type == 'pt':
@@ -186,5 +187,3 @@ async def inference(
     if detected_objects is None:
         return DetectedAndClassifiedObject(object_bbox=None)
     return DetectedAndClassifiedObject(object_bbox=detected_objects)
-
-    # TODO: добавить возможность выбора конфигурации модели
