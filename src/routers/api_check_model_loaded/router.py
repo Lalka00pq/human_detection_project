3aa6f2c3d017afe1c728b9133ac03e1f48f8d106
Lalka00pq <lalka00pq@gmail.com>
2025-05-16 18:57:23 +0300

    Added schema for check model loaded

--- src/routers/api_check_model_loaded/router.py
+++ src/routers/api_check_model_loaded/router.py
@@ -1,3 +1,5 @@
+# project
+from src.schemas.service_output import GetCheckModelLoaded
 # 3rdparty
 from fastapi import APIRouter, Request
 
@@ -8,20 +10,22 @@ router = APIRouter(tags=["Model Loaded Check"])
     "/check_model_loaded",
     summary="Проверяет загружена ли модель",
     description="Проверяет загружена ли модель",
+    response_model=GetCheckModelLoaded,
+    response_description="Датакласс с информацией о загруженности модели",
 )
-def check_model_loaded(request: Request):
+def check_model_loaded(request: Request) -> GetCheckModelLoaded:
     """Проверяет загружена ли модель
 
     Args:
         request (Request): Объект запроса
 
     Returns:
-        _type_: _description_
+        GetCheckModelLoaded: Датакласс с информацией о загруженности модели
     """
     if request.app.state.model is None:
-        return {
-            "Model_loaded": False,
-        }
-    return {
-        "Model_loaded": True,
-    }
+        return GetCheckModelLoaded(
+            Model_loaded=False,
+        )
+    return GetCheckModelLoaded(
+        Model_loaded=True,
+    )
